b8874a8c3da5a7b72f5e1b16a988526648bab39c
Leonardom97 <100780990+Leonardom97@users.noreply.github.com>
2025-12-14 23:49:49 -0500

    v1

---
+++ inc/settings.php
@@ -0,0 +1,23 @@
+<?php
+
+$config = array(
+    'HOST' => 'localhost',
+    'USER' => 'root',
+    'PASS' => '',
+    'DB'   => 'auth',
+    'CORE' => '7878'
+);
+
+
+// General Settings
+define('EXPANSION', 4); // 1 = Vanilla / 2 = TBC / 3 = WOTLK
+define('REALMLIST', 'logon.athenaswow-project.com');
+
+// Google ReCaptcha Settings
+define('CAPTCHA_SECRET', '');
+define('CAPTCHA_CLIENT_ID', '');
+
+// Message Settings
+define('SUCCESS_MESSAGE', 'Descarga del Cliente http://www.mediafire.com/file/hle5eso5ye1gsiw/Athenas_WoW_4.3.4_Minimal.exe');
+
+?>
